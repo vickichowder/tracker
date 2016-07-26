9c36f2b64cb848b79417cb3d336993328b97904e
vickichowder <vechow12@gmail.com>
2016-07-26 17:26:04 -0400

    link function works for fb first time login

--- data/check.py
+++ data/check.py
@@ -8,41 +8,40 @@ from model.position import Position
 from model.tracker import Tracker
 from model.user import User
 
-def in_person_first_time(email, s_media):
+def in_person_first_time(email, media):
     # returns true if email not in db
-    if s_media == 'fb':
+    if media == 'fb':
         return not db.session.query(db.exists().where(User.fb_email == email)).scalar()
-    elif s_media == 'google':
+    elif media == 'google':
         return not db.session.query(db.exists().where(User.google_email == email)).scalar()
 
-def in_person(phone, email, s_media):
+def in_person(phone, email, media):
     # Returns true if user paid in person
     phone_exist = db.session.query(db.exists().where(User.phone == phone)).scalar()
     print(phone_exist)
-    if s_media == 'fb':
+    if media == 'fb':
         user.fb_email = email
-    elif s_media == 'google':
+    elif media == 'google':
         user.google_email = email
     email_exist = db.session.query(db.exists().where(User.email == email)).scalar()
     print(email_exist)
 
     return (phone_exist and not email_exist)
 
-def link(phone, email, s_media):
+def link(phone, user_id, email, media):
     print('link function:')
-    user = User.query.filter(User.phone == phone)
+    user = User.query.filter(User.phone == phone).first()
     print(user)
     # Add their email to the respective login type
-    if s_media == 'fb':
+    if media == 'fb':
         user.fb_email = email
-    elif s_media == 'google':
+    elif media == 'google':
         user.google_email = email
-    print(user.email)
-
+    print(user)
     db.session.commit()
 
     tracker_info = []
-    tracker_info = Tracker.query.with_entities(Tracker.tracker_id).filter(Tracker.email == email)
+    tracker_info = Tracker.query.with_entities(Tracker.tracker_id).filter(Tracker.user_id == user.user_id)
     print(tracker_info)
     for row in tracker_info:
         tracker = dict(row.__dict__)
@@ -51,3 +50,9 @@ def link(phone, email, s_media):
         tracker_info.append(tracker)
 
     return tracker_info
+
+def get_user_id(email, media):
+    if media == 'fb':
+        return User.query.filter(User.fb_email == email).first().user_id
+    elif media == 'google':
+        return User.query.filter(User.google_email == email).first().user_id
